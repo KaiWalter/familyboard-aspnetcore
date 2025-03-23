5e4245009d03d24ab778cae65bdd97110f232bdb
Kai Walter <kai.walter@outlook.de>
2025-03-23 18:43:48 +0100

    change image from background to regular

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,48 +2,49 @@
 var imageUpdateCounter = 1;
 
 function startTime() {
-    var today = new Date();
-    document.querySelector('fb-clock').time = {
-        hh: today.getHours().toString().padStart(2, "0"),
-        mm: today.getMinutes().toString().padStart(2, "0"),
-        ss: today.getSeconds().toString().padStart(2, "0")
-    }
-    var t = setTimeout(startTime, 500);
+  var today = new Date();
+  document.querySelector("fb-clock").time = {
+    hh: today.getHours().toString().padStart(2, "0"),
+    mm: today.getMinutes().toString().padStart(2, "0"),
+    ss: today.getSeconds().toString().padStart(2, "0"),
+  };
+  var t = setTimeout(startTime, 500);
 }
 
 function startMainLoop() {
-    MainLoop();
+  MainLoop();
 }
 
 function MainLoop() {
+  calendarUpdateCounter--;
 
-    calendarUpdateCounter--;
+  if (calendarUpdateCounter <= 0) {
+    console.log("update calendar");
+    updateCalendar();
+    calendarUpdateCounter = 300;
+  }
 
-    if (calendarUpdateCounter <= 0) {
-        console.log('update calendar');
-        updateCalendar();
-        calendarUpdateCounter = 300;
-    }
+  imageUpdateCounter--;
 
-    imageUpdateCounter--;
+  if (imageUpdateCounter <= 0) {
+    console.log("update image");
+    updateImage();
+    imageUpdateCounter = 90;
+  }
 
-    if (imageUpdateCounter <= 0) {
-        console.log('update image');
-        updateImage();
-        imageUpdateCounter = 90;
-    }
+  putStatus(
+    `next image update ${imageUpdateCounter}s - next calendar update ${calendarUpdateCounter}s`,
+  );
 
-    putStatus(`next image update ${imageUpdateCounter}s - next calendar update ${calendarUpdateCounter}s`);
-
-    var t = setTimeout(MainLoop, 1000);
+  var t = setTimeout(MainLoop, 1000);
 }
 
 function putMessage(message) {
-    document.querySelector('fb-message').content = message;
+  document.querySelector("fb-message").content = message;
 }
 
 function putStatus(status) {
-    document.querySelector('fb-status').content = status;
+  document.querySelector("fb-status").content = status;
 }
 
 // --------------------------------------------------------------------------------
@@ -53,155 +54,194 @@ let monthNames;
 let weekDayNames;
 
 function initCalendar() {
-    if (!monthNames || !weekDayNames) {
-        fetch("/api/calendar/dateformatinfo")
-            .then(response => response.json())
-            .then(data => {
-                if (data) {
-                    monthNames = data.monthNames;
-                    weekDayNames = data.weekDayNames;
-                }
-            });
-    }
+  if (!monthNames || !weekDayNames) {
+    fetch("/api/calendar/dateformatinfo")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          monthNames = data.monthNames;
+          weekDayNames = data.weekDayNames;
+        }
+      });
+  }
 }
 
 function updateCalendar() {
-    initCalendar();
+  initCalendar();
 
-    fetch("/api/calendar")
-        .then(response => response.json())
-        .then(data => {
-            if (data) {
-                renderCalendar(data);
-            }
-        });
-
+  fetch("/api/calendar")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        renderCalendar(data);
+      }
+    });
 }
 
 function ISO8601_week_no(dt) {
-    var tdt = new Date(dt.valueOf());
-    var dayn = (dt.getDay() + 6) % 7;
-    tdt.setDate(tdt.getDate() - dayn + 3);
-    var firstThursday = tdt.valueOf();
-    tdt.setMonth(0, 1);
-    if (tdt.getDay() !== 4) {
-        tdt.setMonth(0, 1 + ((4 - tdt.getDay()) + 7) % 7);
-    }
-    return 1 + Math.ceil((firstThursday - tdt) / 604800000);
+  var tdt = new Date(dt.valueOf());
+  var dayn = (dt.getDay() + 6) % 7;
+  tdt.setDate(tdt.getDate() - dayn + 3);
+  var firstThursday = tdt.valueOf();
+  tdt.setMonth(0, 1);
+  if (tdt.getDay() !== 4) {
+    tdt.setMonth(0, 1 + ((4 - tdt.getDay() + 7) % 7));
+  }
+  return 1 + Math.ceil((firstThursday - tdt) / 604800000);
 }
 
 function ISO8601_date(dt) {
-    return dt.getFullYear().toString() + "-" +
-        (dt.getMonth() + 1).toString().padStart(2, "0") + "-" +
-        dt.getDate().toString().padStart(2, "0");
+  return (
+    dt.getFullYear().toString() +
+    "-" +
+    (dt.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    dt.getDate().toString().padStart(2, "0")
+  );
 }
 
 function addDays(date, days) {
-    var result = new Date(date);
-    result.setDate(result.getDate() + days);
-    return result;
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
 }
 
 function currentDate() {
-    let current = new Date();
-    current.setHours(0, 0, 0, 0)
-    return current;
+  let current = new Date();
+  current.setHours(0, 0, 0, 0);
+  return current;
 }
 
 function renderCalendar(events) {
-    putMessage("calendar refresh");
+  putMessage("calendar refresh");
 
-    // find first day of week
-    let current = currentDate();
+  // find first day of week
+  let current = currentDate();
 
-    let wd = current.getDay();
-    let firstDate = currentDate();
-    // flip week on Sunday
-    if (wd === 0) {
-        firstDate = addDays(firstDate, -1);
-        wd = firstDate.getDay();
-    }
-    // find Monday
-    while (wd > 1) {
-        firstDate = addDays(firstDate, -1);
-        wd = firstDate.getDay();
-    }
+  let wd = current.getDay();
+  let firstDate = currentDate();
+  // flip week on Sunday
+  if (wd === 0) {
+    firstDate = addDays(firstDate, -1);
+    wd = firstDate.getDay();
+  }
+  // find Monday
+  while (wd > 1) {
+    firstDate = addDays(firstDate, -1);
+    wd = firstDate.getDay();
+  }
 
-    // fill calendar
-    let iDate = currentDate();
-    let data = [];
+  // fill calendar
+  let iDate = currentDate();
+  let data = [];
 
-    for (w = 0; w < 3; w++) {
-        let iDate = addDays(firstDate, w * 7);
-        let weekData = { weekNo: ISO8601_week_no(iDate).toString().padStart(2, "0"), days: [] };
+  for (w = 0; w < 3; w++) {
+    let iDate = addDays(firstDate, w * 7);
+    let weekData = {
+      weekNo: ISO8601_week_no(iDate).toString().padStart(2, "0"),
+      days: [],
+    };
 
-        for (wd = 0; wd < 7; wd++) {
-            let i = (w * 7) + wd;
-            let iDate = addDays(firstDate, i);
-            let isToday = ISO8601_date(iDate) === ISO8601_date(current);
+    for (wd = 0; wd < 7; wd++) {
+      let i = w * 7 + wd;
+      let iDate = addDays(firstDate, i);
+      let isToday = ISO8601_date(iDate) === ISO8601_date(current);
 
-            let monthTitle = "";
-            if (i === 0 || iDate.getDate() == 1) {
-                monthTitle = monthNames[iDate.getMonth()];
-            }
+      let monthTitle = "";
+      if (i === 0 || iDate.getDate() == 1) {
+        monthTitle = monthNames[iDate.getMonth()];
+      }
 
-            let dayData = { monthOfYear: monthTitle, dayOfWeek: weekDayNames[wd], dayOfMonth: iDate.getDate().toString().padStart(2, "0"), isToday: isToday, events: [] };
-            let iDateFormatted = ISO8601_date(iDate);
+      let dayData = {
+        monthOfYear: monthTitle,
+        dayOfWeek: weekDayNames[wd],
+        dayOfMonth: iDate.getDate().toString().padStart(2, "0"),
+        isToday: isToday,
+        events: [],
+      };
+      let iDateFormatted = ISO8601_date(iDate);
 
-            // render all day events always on top
-            events.forEach((entry) => {
-                if (iDateFormatted === entry.date && entry.allDayEvent) {
-                    let eventData = { description: entry.description, isPrimary: entry.isPrimary, isSecondary: entry.isSecondary, allDayEvent: entry.allDayEvent, publicHoliday: entry.publicHoliday, schoolHoliday: entry.schoolHoliday };
-                    dayData.events.push(eventData);
-                }
-            });
-
-            // render timed events below
-            events.forEach((entry) => {
-                if (iDateFormatted === entry.date && !entry.allDayEvent) {
-                    let eventData = { description: entry.description, time: entry.time, isPrimary: entry.isPrimary, isSecondary: entry.isSecondary, allDayEvent: entry.allDayEvent, publicHoliday: entry.publicHoliday, schoolHoliday: entry.schoolHoliday };
-                    dayData.events.push(eventData);
-                }
-            });
-
-            weekData.days.push(dayData);
+      // render all day events always on top
+      events.forEach((entry) => {
+        if (iDateFormatted === entry.date && entry.allDayEvent) {
+          let eventData = {
+            description: entry.description,
+            isPrimary: entry.isPrimary,
+            isSecondary: entry.isSecondary,
+            allDayEvent: entry.allDayEvent,
+            publicHoliday: entry.publicHoliday,
+            schoolHoliday: entry.schoolHoliday,
+          };
+          dayData.events.push(eventData);
         }
+      });
 
-        data.push(weekData);
+      // render timed events below
+      events.forEach((entry) => {
+        if (iDateFormatted === entry.date && !entry.allDayEvent) {
+          let eventData = {
+            description: entry.description,
+            time: entry.time,
+            isPrimary: entry.isPrimary,
+            isSecondary: entry.isSecondary,
+            allDayEvent: entry.allDayEvent,
+            publicHoliday: entry.publicHoliday,
+            schoolHoliday: entry.schoolHoliday,
+          };
+          dayData.events.push(eventData);
+        }
+      });
+
+      weekData.days.push(dayData);
     }
 
-    document.querySelector('fb-calendar').data = data;
+    data.push(weekData);
+  }
 
-    putMessage("");
+  document.querySelector("fb-calendar").data = data;
+
+  putMessage("");
 }
 
 // --------------------------------------------------------------------------------
 // update image
 
 function updateImage() {
-    fetch("/api/image")
-        .then(response => response.json())
-        .then(data => renderImage(data));
+  fetch("/api/image")
+    .then((response) => response.json())
+    .then((data) => renderImage(data));
 }
 
 function renderImage(imageObj) {
-    putMessage("updating image");
+  putMessage("updating image");
 
-    var img = new Image();
-    img.onload = function() {
-        imgContainer = document.getElementsByClassName('imageContainer')[0];
-        imgContainer.style.background = "#000 url(" + imageObj.src + ") center center";
-        imgContainer.style.backgroundSize = "cover";
-        imgContainer.style.backgroundRepeat = "no-repeat";
+  var img = new Image();
+  img.onload = function () {
+    // imgContainer = document.getElementsByClassName('imageContainer')[0];
+    // imgContainer.style.background = "#000 url(" + imageObj.src + ") center center";
+    // imgContainer.style.backgroundSize = "cover";
+    // imgContainer.style.backgroundRepeat = "no-repeat";
 
-        imgCreated = document.getElementsByClassName('imageCreated')[0];
-        var imageCreatedLabel = "";
-        if (imageObj.month && imageObj.year) {
-            imageCreatedLabel = monthNames[imageObj.month - 1] + " " + imageObj.year;
-        }
-        imgCreated.innerHTML = imageCreatedLabel;
+    imgContainer = document.getElementsByClassName("imageContainer")[0];
+
+    const imgExisting = imgContainer.querySelector("img");
+    if (imgExisting) {
+      imageContainer.removeChild(imgExisting);
     }
-    img.src = imageObj.src;
 
-    putMessage("");
+    imgContainer.appendChild(img);
+    img.style.width = "100%";
+    img.style.height = "100%";
+    img.style.objectFit = "cover";
+
+    imgCreated = document.getElementsByClassName("imageCreated")[0];
+    var imageCreatedLabel = "";
+    if (imageObj.month && imageObj.year) {
+      imageCreatedLabel = monthNames[imageObj.month - 1] + " " + imageObj.year;
+    }
+    imgCreated.innerHTML = imageCreatedLabel;
+  };
+  img.src = imageObj.src;
+
+  putMessage("");
 }
